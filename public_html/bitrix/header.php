c396ff1a93c81f1b587a5bb0365a1a8d4167cc73
Katy <katena.moskvina@bk.ru>
2021-06-21 14:46:34 +0600

    first commit

---
+++ public_html/bitrix/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+
+<body>
+    <div class="wrapper">
+        <header class="header"></header>
---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+<?
+	require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+?>
+
+<main class="">
+	life-garden
+</main>
+
+<?
+	require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+?>
